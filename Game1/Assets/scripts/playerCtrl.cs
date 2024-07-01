9ae8fb3c0f79349bd2c73a63fb698a218537c44b
Ben Geo Abraham <bengeoabraham@gmail.com>
2024-07-02 03:42:51 +0530

    Dash

--- Game1/Assets/scripts/playerCtrl.cs
+++ Game1/Assets/scripts/playerCtrl.cs
@@ -11,7 +11,13 @@ public class playerCtrl : MonoBehaviour
     public float speed;
     public float horizontal;
     public float jumpingPower;
-    private bool isLookingRight;
+    private bool isFacingRight = true;
+
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 300f;
+    private float dashingTime = 0.1f;
+    private float dashingCooldown = 1f;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
@@ -32,15 +38,23 @@ public class playerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
         //movement
         horizontal = Input.GetAxisRaw("Horizontal");
         
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
         {
-            Debug.Log("Space");
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-           
         }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("h");
+            StartCoroutine(Dash());
+        }
+        Flip();
     }
     private bool isGrounded()
     {
@@ -48,11 +62,36 @@ public class playerCtrl : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        if (isDashing)
+        {
+            return;
+        }
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
-    private void Dodge()
+    private void Flip()
     {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            Vector3 localScale = transform.localScale;
+            isFacingRight = !isFacingRight;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+    private IEnumerator Dash()
+    {
+        
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
 
+        yield return new WaitForSeconds(dashingTime);
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 
 
